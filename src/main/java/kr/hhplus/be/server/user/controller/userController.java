9775e24f3cf3662f228d3632544b81f89d5f737d
Song-Jae-Hong <woghd1313@naver.com>
2025-07-18 00:52:06 +0900

    유저 잔액 조회 및 충전 API

--- src/main/java/kr/hhplus/be/server/user/controller/userController.java
+++ src/main/java/kr/hhplus/be/server/user/controller/userController.java
@@ -1,8 +1,27 @@
 package kr.hhplus.be.server.user.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+import java.util.Map;
 
+@RestController
+@RequestMapping("/api/users")
+@Tag(name = "유저 API", description = "포인트 조회 및 충전 API")
 public class userController {
 
+    @Operation(summary = "포인트 조회", description = "포인트 잔액를 조회합니다.")
+    @GetMapping("/{userId}/balance")
+    public ResponseEntity<Map<String, Object>> getBalance(@PathVariable String userId) {
+        return ResponseEntity.ok(Map.of("balance", 25000));
+    }
+
+    @Operation(summary = "포인트 충전", description = "포인트를 충전합니다.")
+    @PostMapping("/{userId}/balance/charge")
+    public ResponseEntity<Map<String, Object>> chargeBalance(@PathVariable String userId, @RequestBody Map<String, Integer> request) {
+        return ResponseEntity.ok(Map.of("balance", 75000));
+    }
 }
