bf394b6015929da25faa80bf3df47524873438dd
Song-Jae-Hong <woghd1313@naver.com>
2025-07-18 00:52:21 +0900

    결제 API

--- src/main/java/kr/hhplus/be/server/payment/controller/paymentController.java
+++ src/main/java/kr/hhplus/be/server/payment/controller/paymentController.java
@@ -1,7 +1,25 @@
 package kr.hhplus.be.server.payment.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/payments")
+@Tag(name = "결제 API", description = "좌석 결제 API")
 public class paymentController {
 
-
+    @Operation(summary = "결제 요청", description = "결제를 진행합니다.")
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> processPayment(@RequestBody Map<String, Object> request) {
+        return ResponseEntity.ok(Map.of(
+                "paymentId", 5678,
+                "amount", 30000,
+                "status", "SUCCESS"
+        ));
+    }
 }
